c5bcac77efea1d968ffb7ceee700fc8556564b7f
Toby Parsons <toby.d.parsons@gmail.com>
2024-04-13 21:33:09 +0100

    Add typing functionality
    Add backspace ability
    Update functions

--- script.js
+++ script.js
@@ -3,24 +3,48 @@ const decimalBtn = document.querySelector(".decimal");
 const opBtns = document.querySelectorAll(".operator");
 const equalsBtn = document.querySelector(".equals");
 const clearBtn = document.querySelector(".clear");
+const backspace = document.querySelector(".backspace-container");
 let display = document.querySelector("#display");
 let a;
 let operator;
 let b;
 
-function enterDigit() {  
+function shouldAddDigit() {
+    if (display.textContent.length <= 16) {
+        return true
+    }
+}
+
+function enterDigit(num) {  
     if (typeof a !== 'undefined' && typeof operator === 'undefined') {
             a = undefined;
         }
         if (typeof b === 'undefined') {
-            display.textContent = this.textContent;
-        } else if (display.textContent == '0' && this.textContent == '0') {
-            console.log(this.textContent); 
+            display.textContent = num;
+        } else if (display.textContent == '0' && num == '0') {
+            console.log(num); 
         } else {
-            display.textContent += this.textContent;
-            console.log(this.textContent);
+            display.textContent += num;
+            console.log(num);
         }
         b = Number(display.textContent);
+}
+
+function shouldBackspace() {
+    if (typeof b !== 'undefined' && b !== 0 && display.textContent !== '0.') {
+        return true
+    } else {
+        return false
+    }
+}
+
+function removeLast() {
+    display.textContent = display.textContent.slice(0, -1);
+    b = Number(display.textContent);
+    if (b == '') {
+        b = undefined;
+        display.textContent = '0';
+    }
 }
 
 function shouldAddDecimal() {
@@ -42,27 +66,38 @@ function addDecimal() {
     b = Number(display.textContent);
 }
 
-function clickOperator() {
-    if (typeof b === 'undefined') {
-        operator = this.textContent;
+function clickOperator(op) {
+    debugger;
+    if (typeof a === 'undefined' && typeof b === 'undefined') {
+        a = 0;
+        operator = op;
+    } else if (typeof b === 'undefined') {
+        operator = op;
     } else if (typeof a === 'undefined') {
         a = b;
         b = undefined;
-        operator = this.textContent;
+        operator = op;
     } else {
         a = operate(a, operator, b);
         display.textContent = a;
         b = undefined;
-        operator = this.textContent;
+        operator = op;
     }
 }
 
 function calculate() {
-    b = Number(display.textContent);
-    display.textContent = operate(a, operator, b);
-    a = Number(display.textContent);
-    b = undefined;
-    operator = undefined;
+    if (typeof a === 'undefined' && typeof b === 'undefined') {
+        display.textContent = '0';
+    } else if (typeof operator == 'undefined') {
+        a = b;
+        b = undefined;
+    } else {
+        b = Number(display.textContent);
+        display.textContent = operate(a, operator, b);
+        a = Number(display.textContent);
+        b = undefined;
+        operator = undefined;
+    }
 }
 
 function clearCalc() {
@@ -87,37 +122,78 @@ function operate(a, operator, b){
     }
 }
 
-digitBtns.forEach((btn) => btn.addEventListener("click", enterDigit));
+digitBtns.forEach((btn) => btn.addEventListener("click", function() {
+    debugger;
+    shouldAddDigit() ? enterDigit(btn.textContent) : false;
+}));
 decimalBtn.addEventListener("click", () => shouldAddDecimal() ? addDecimal() : false);
-opBtns.forEach((btn) => btn.addEventListener("click", clickOperator));
-equalsBtn.addEventListener("click", calculate)
-clearBtn.addEventListener("click", clearCalc)
+opBtns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        clickOperator(event.currentTarget.textContent);
+    });
+});
+equalsBtn.addEventListener("click", calculate);
+clearBtn.addEventListener("click", clearCalc);
+backspace.addEventListener("click", () => shouldBackspace() ? removeLast() : false);
 
-
-/*
-operate = (a, operator, b) => {
-    let result = 0;
-    switch(operator) {
-        case add:
-            result = a + b;
+document.addEventListener("keydown", (event) => {
+    switch(event.key) {
+        case '.':
+            shouldAddDecimal() ? addDecimal() : false;
             break;
-        case subtract:
-            result = a - b;
+        case 'Backspace':
+            shouldBackspace() ? removeLast() : false;
             break;
-        case multiply:
-            result = a * b;
+        case 'Delete':
+            clearCalc();
             break;
-        case divide:
-            result = a / b;
+        case '=':
+            calculate();
+            break;
+        case 'Enter':
+            calculate();
+            break;
+        case '+':
+            clickOperator(event.key);
+            break;
+        case '-':
+            clickOperator(event.key);
+            break;
+        case '*':
+            clickOperator(event.key);
+            break;
+        case '/':
+            clickOperator(event.key);
+            break;
+        case '1':
+            shouldAddDigit() ? enterDigit(event.key) : false;
+            break;
+        case '2':
+            shouldAddDigit() ? enterDigit(event.key) : false;
+            break;
+        case '3':
+            shouldAddDigit() ? enterDigit(event.key) : false;
+            break;
+        case '4':
+            shouldAddDigit() ? enterDigit(event.key) : false;
+            break;
+        case '5':
+            shouldAddDigit() ? enterDigit(event.key) : false;
+            break;
+        case '6':
+            shouldAddDigit() ? enterDigit(event.key) : false;
+            break;
+        case '7':
+            shouldAddDigit() ? enterDigit(event.key) : false;
+            break;
+        case '8':
+            shouldAddDigit() ? enterDigit(event.key) : false;
+            break;
+        case '9':
+            shouldAddDigit() ? enterDigit(event.key) : false;
+            break;
+        case '0':
+            shouldAddDigit() ? enterDigit(event.key) : false;
             break;
     }
-    return result;
-}
-*/
-
-
-
-/* add = (a, b) => a + b;
-subtract = (a, b) => a - b;
-multiply = (a, b) => a * b;
-divide = (a, b) => a / b; */
+})
